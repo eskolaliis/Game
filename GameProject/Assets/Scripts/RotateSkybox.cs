2b47aae777e8e34dffa42aa0c1983aef89f510d6
eskolaliis <168006493+eskolaliis@users.noreply.github.com>
2024-12-02 17:00:29 +0200

    MainMenu

--- GameProject/Assets/Scripts/RotateSkybox.cs
+++ GameProject/Assets/Scripts/RotateSkybox.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RotateSkybox : MonoBehaviour
 {
-    public float rotationSpeed = 1.0f; // Pyörityksen nopeus
+    public float rotationSpeed = 1.0f; // Skyboxin pyörityksen nopeus
 
     void Update()
-    
     {
-        // Asettaa Skyboxin pyörimään jatkuvasti ajan mukaan
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        // Tarkista, että aktiivinen Scene on "MainMenu"
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            // Pyöritä Skyboxia
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        }
     }
 }
-
-
-
