33c0be58c0b8482656f584f17d97bed469f8dd5d
amaatta1 <anne.maatta@edu.lapinamk.fi>
2024-12-07 00:05:11 +0200

    kentän2viimeistelyt

---
+++ GameProject/Assets/Scripts AulaScene/Tavara.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Tavara : MonoBehaviour
+{
+    public GameObject ItemPlace;
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown kutsuttiin!");
+
+    
+
+        // Aktivoi objekti
+        if (ItemPlace != null)
+        {
+            ItemPlace.SetActive(true);
+            Debug.Log("ItemPlace asetettiin n‰kyv‰ksi.");
+        }
+        else
+        {
+            Debug.LogError("ItemPlace-objekti ei ole m‰‰ritetty!");
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
