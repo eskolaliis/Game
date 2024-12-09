a458e11e8a1251e6f7165f2f5e08237f838d8003
alinakokko <alikokko@edu.lapinamk.fi>
2024-12-09 22:44:05 +0200

    Hint painike lisätty

--- GameProject/Assets/Scripts ITopetScene/Exit.cs
+++ GameProject/Assets/Scripts ITopetScene/Exit.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
    public void ExitBtnClicked()
     {
         SceneManager.LoadScene("MainMenu");
---
+++ GameProject/Assets/Scripts ITopetScene/HintText.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintText : MonoBehaviour
+{
+    public TextMeshPro panelText;
+
+    public void HintBtnClicked()
+    {
+        panelText.text = "Löydä kirjoja päästäksesi eroon Yrjöstä";
+    }
+}
